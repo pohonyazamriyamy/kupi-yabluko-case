735bdcb3717933d0bf4b8fba91515bdb28216776
pohonyazamriyamy <pohonya.za.mriyamy@gmail.com>
2026-07-16 23:00:59 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.PRINT_CASE_CONFIG = {
   siteName: "Купи Яблуко — друк фото на чохлах",
   instagramUrl: "https://www.instagram.com/kupi_yabluko_lutsk",
-  telegramUrl: "https://t.me/",
+  telegramUrl: "https://t.me/Kupi_Yabluko_L",
   phone: "",
   logoUrl: "",
   backgroundImageUrl: "",
